cddc66a104bb61b7c22cd547d4cc10f162a1cc24
Doan Hong Ngoc <ngocdh236@gmail.com>
2019-09-24 16:12:45 +0300

    Example test for CI checking

---
+++ test/server.js
@@ -0,0 +1,18 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+var app = require('../server.js');
+
+const { expect } = chai;
+chai.use(chaiHttp);
+
+describe('GET /', function() {
+  it('respond with hello world', function(done) {
+    chai
+      .request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res.text).equals('Hello World');
+        done();
+      });
+  });
+});
